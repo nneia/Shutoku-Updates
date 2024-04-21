020d1ec92e9176f71b9a99ea76c7e991dbcc4ca2
nneia <bazhirov.work@gmail.com>
2024-04-21 20:49:15 +0900

    className Update

--- src/components/ListContainer/ListContainer.jsx
+++ src/components/ListContainer/ListContainer.jsx
@@ -2,137 +2,149 @@ import "./ListContainer.css";
 const linkToPA = "https://maps.app.goo.gl/caxDBY1KC52nDLj97";
 
 const ListContainer = ({ openItem, openModal }) => (
-  <div class="list-container">
-    <div class="list-item">
+  <div className="list-container">
+    <div className="list-item">
       <img
         src="src/assets/imgs/tatsumi1-img1.jpg"
         alt=""
-        class="list-img"
+        className="list-img"
         onClick={openItem}
       />
-      <div class="list-info">
-        <h2 class="list-title" onClick={openItem}>
+      <div className="list-info">
+        <h2 className="list-title" onClick={openItem}>
           Tatsumi 1 PA (IN)
         </h2>
-        <div class="list-status-box">
-          <p class="list-status">Open</p>
-          <div class="list-status-icon open"></div>
+        <div className="list-status-box">
+          <p className="list-status">Open</p>
+          <div className="list-status-icon open"></div>
         </div>
-        <div class="list-controls">
-          <p class="list-update" onClick={openModal}>
+        <div className="list-controls">
+          <p className="list-update" onClick={openModal}>
             Update Status
           </p>
-          <a href={linkToPA} target="_blank">
-            <div class="list-nav">
+          <a href={linkToPA} target="_blank" rel="noopener noreferrer">
+            <div className="list-nav">
               <img
                 src="src/assets/icons/directions.jpg"
                 alt=""
-                class="nav-icon"
+                className="nav-icon"
               />
             </div>
           </a>
         </div>
       </div>
     </div>
-    <div class="list-item">
+    <div className="list-item">
+      <img src="src/assets/imgs/daikoku-img1.jpg" alt="" className="list-img" />
+      <div className="list-info">
+        <h2 className="list-title">Daikoku PA</h2>
+        <div className="list-status-box">
+          <p className="list-status">Open</p>
+          <div className="list-status-icon open"></div>
+        </div>
+        <div className="list-controls">
+          <p className="list-update">Update Status</p>
+          <div className="list-nav">
+            <img
+              src="src/assets/icons/directions.jpg"
+              alt=""
+              className="nav-icon"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="list-item">
       <img
-        src={`${import.meta.env.BASE_URL}src/assets/imgs/tatsumi1-img1.jpg`}
+        src="src/assets/imgs/heiwajima-img1.jpg"
         alt=""
-        class="list-img"
+        className="list-img"
       />
-      <div class="list-info">
-        <h2 class="list-title">Daikoku PA</h2>
-        <div class="list-status-box">
-          <p class="list-status">Open</p>
-          <div class="list-status-icon open"></div>
+      <div className="list-info">
+        <h2 className="list-title">Heiwajima PA (OUT)</h2>
+        <div className="list-status-box">
+          <p className="list-status">Open</p>
+          <div className="list-status-icon open"></div>
         </div>
-        <div class="list-controls">
-          <p class="list-update">Update Status</p>
-          <div class="list-nav">
+        <div className="list-controls">
+          <p className="list-update">Update Status</p>
+          <div className="list-nav">
             <img
               src="src/assets/icons/directions.jpg"
               alt=""
-              class="nav-icon"
+              className="nav-icon"
             />
           </div>
         </div>
       </div>
     </div>
-    <div class="list-item">
-      <img src="src/assets/imgs/heiwajima-img1.jpg" alt="" class="list-img" />
-      <div class="list-info">
-        <h2 class="list-title">Heiwajima PA (OUT)</h2>
-        <div class="list-status-box">
-          <p class="list-status">Open</p>
-          <div class="list-status-icon open"></div>
+    <div className="list-item">
+      <img
+        src="src/assets/imgs/shibaura-img1.jpg"
+        alt=""
+        className="list-img"
+      />
+      <div className="list-info">
+        <h2 className="list-title">Shibaura PA</h2>
+        <div className="list-status-box">
+          <p className="list-status">Open</p>
+          <div className="list-status-icon open"></div>
         </div>
-        <div class="list-controls">
-          <p class="list-update">Update Status</p>
-          <div class="list-nav">
+        <div className="list-controls">
+          <p className="list-update">Update Status</p>
+          <div className="list-nav">
             <img
               src="src/assets/icons/directions.jpg"
               alt=""
-              class="nav-icon"
+              className="nav-icon"
             />
           </div>
         </div>
       </div>
     </div>
-    <div class="list-item">
-      <img src="src/assets/imgs/shibaura-img1.jpg" alt="" class="list-img" />
-      <div class="list-info">
-        <h2 class="list-title">Shibaura PA</h2>
-        <div class="list-status-box">
-          <p class="list-status">Open</p>
-          <div class="list-status-icon open"></div>
+    <div className="list-item">
+      <img
+        src="src/assets/imgs/hakozaki-img1.jpg"
+        alt=""
+        className="list-img"
+      />
+      <div className="list-info">
+        <h2 className="list-title">Hakozaki PA</h2>
+        <div className="list-status-box">
+          <p className="list-status">Open</p>
+          <div className="list-status-icon open"></div>
         </div>
-        <div class="list-controls">
-          <p class="list-update">Update Status</p>
-          <div class="list-nav">
+        <div className="list-controls">
+          <p className="list-update">Update Status</p>
+          <div className="list-nav">
             <img
               src="src/assets/icons/directions.jpg"
               alt=""
-              class="nav-icon"
+              className="nav-icon"
             />
           </div>
         </div>
       </div>
     </div>
-    <div class="list-item">
-      <img src="src/assets/imgs/hakozaki-img1.jpg" alt="" class="list-img" />
-      <div class="list-info">
-        <h2 class="list-title">Hakozaki PA</h2>
-        <div class="list-status-box">
-          <p class="list-status">Open</p>
-          <div class="list-status-icon open"></div>
+    <div className="list-item">
+      <img
+        src="src/assets/imgs/umihotaru-img1.jpg"
+        alt=""
+        className="list-img"
+      />
+      <div className="list-info">
+        <h2 className="list-title">Umihotaru PA</h2>
+        <div className="list-status-box">
+          <p className="list-status">Open</p>
+          <div className="list-status-icon open"></div>
         </div>
-        <div class="list-controls">
-          <p class="list-update">Update Status</p>
-          <div class="list-nav">
+        <div className="list-controls">
+          <p className="list-update">Update Status</p>
+          <div className="list-nav">
             <img
               src="src/assets/icons/directions.jpg"
               alt=""
-              class="nav-icon"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="list-item">
-      <img src="src/assets/imgs/umihotaru-img1.jpg" alt="" class="list-img" />
-      <div class="list-info">
-        <h2 class="list-title">Umihotaru PA</h2>
-        <div class="list-status-box">
-          <p class="list-status">Open</p>
-          <div class="list-status-icon open"></div>
-        </div>
-        <div class="list-controls">
-          <p class="list-update">Update Status</p>
-          <div class="list-nav">
-            <img
-              src="src/assets/icons/directions.jpg"
-              alt=""
-              class="nav-icon"
+              className="nav-icon"
             />
           </div>
         </div>
